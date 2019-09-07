d86cd83cbd12b4219bbd749ff9be2bf080c7ffd8
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2019-09-07 22:24:10 +0100

    Implemented example

--- src/main/java/io/github/lexesjan/machinelearning/examples/MNIST.java
+++ src/main/java/io/github/lexesjan/machinelearning/examples/MNIST.java
@@ -1,4 +1,24 @@
 package io.github.lexesjan.machinelearning.examples;
 
+import io.github.lexesjan.machinelearning.NeuralNetwork;
+import io.github.lexesjan.machinelearning.datawrapper.Data;
+import io.github.lexesjan.machinelearning.datawrapper.Image;
+import io.github.lexesjan.machinelearning.util.MNISTLoader;
+
+import java.io.IOException;
+
 public class MNIST {
+    public static void main(String[] args) throws IOException {
+        try {
+            long start = System.currentTimeMillis();
+            Data[][] MNISTData = MNISTLoader.loadDataWrapper("mnist dataset");
+            System.out.println("Finished loading data in " + (System.currentTimeMillis() - start) + " ms.");
+            Data[] trainingData = MNISTData[0];
+            Data[] testData = MNISTData[1];
+            NeuralNetwork nn = new NeuralNetwork(new int[] {((Image) trainingData[0]).numRows(), 30, 10});
+            nn.train(30, 3, 10, trainingData, testData);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/io/github/lexesjan/machinelearning/examples/XOR.java
+++ src/main/java/io/github/lexesjan/machinelearning/examples/XOR.java
@@ -1,4 +1,22 @@
 package io.github.lexesjan.machinelearning.examples;
 
+import io.github.lexesjan.machinelearning.NeuralNetwork;
+import io.github.lexesjan.machinelearning.datawrapper.Data;
+import org.ejml.simple.SimpleMatrix;
+
 public class XOR {
+    public static void main(String[] args) {
+        Data[] trainingData = {
+                new Data(new SimpleMatrix(new float[][] {{0}, {0}}), new SimpleMatrix(new float[][] {{0}})),
+                new Data(new SimpleMatrix(new float[][] {{0}, {1}}), new SimpleMatrix(new float[][] {{1}})),
+                new Data(new SimpleMatrix(new float[][] {{1}, {0}}), new SimpleMatrix(new float[][] {{1}})),
+                new Data(new SimpleMatrix(new float[][] {{1}, {1}}), new SimpleMatrix(new float[][] {{0}})),
+        };
+        NeuralNetwork nn = new NeuralNetwork(new int[] {2, 10, 1});
+        nn.train(10000, 3, 1, trainingData, trainingData);
+        System.out.println(nn.feedForward(new SimpleMatrix(new float[][] {{0}, {0}})));
+        System.out.println(nn.feedForward(new SimpleMatrix(new float[][] {{0}, {1}})));
+        System.out.println(nn.feedForward(new SimpleMatrix(new float[][] {{1}, {0}})));
+        System.out.println(nn.feedForward(new SimpleMatrix(new float[][] {{1}, {1}})));
+    }
 }
